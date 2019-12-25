317600036703612418499d38d4572bf633bf6aa9
Desmond <desmond.yan.lai@gmail.com>
2019-12-25 14:39:09 -0500

    updated icon section

--- src/Icons.js
+++ src/Icons.js
@@ -8,11 +8,30 @@ library.add(fab)
 const Icons = () => {
 	return (
 		<div className='icons'>
-			 <FontAwesomeIcon className='html5' icon={['fab', 'html5']} />
-			 <FontAwesomeIcon className='css3' icon={['fab', 'css3-alt']} />
-			 <FontAwesomeIcon className='js' icon={['fab', 'js-square']} />
-			 <FontAwesomeIcon className='react' icon={['fab', 'react']} />
+			<div className='pie'>
+				<div className='innerCircle'></div>
+				<div className='triangle90'></div>
+				<FontAwesomeIcon className='html5' icon={['fab', 'html5']} />
+			</div>
+			<div className='pie'>
+				<div className='innerCircle'></div>
+				<div className='triangle90'></div>
+				<FontAwesomeIcon className='css3' icon={['fab', 'css3-alt']} />
+			</div>
+			<div className='pie'>
+				<div className='innerCircle'></div>
+				<div className='triangle80'></div>
+				<FontAwesomeIcon className='js' icon={['fab', 'js-square']} />
+			</div>
+			<div className='pie'>
+				<div className='innerCircle'></div>
+				<div className='triangle80'></div>
+				<FontAwesomeIcon className='react' icon={['fab', 'react']} />			</div>
+			<div className='pie'>
+				<div className='innerCircle'></div>
+				<div className='triangle80'></div>
 			 <FontAwesomeIcon className='github' icon={['fab', 'github-square']} />
+			</div>
 		</div>
 	)
 }
