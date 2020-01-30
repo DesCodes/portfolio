e88ab8d5d90301e028ac3e225e4e10232bb0618c
Desmond <desmond.yan.lai@gmail.com>
2020-01-29 20:18:46 -0500

    Updates

---
+++ precache-manifest.7a48a43cd9f87bd955d3cc2dded3c3ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ed5a10f65ae0a9341dd3f7b8ae8d8f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8fb66853ec0fc650242e",
+    "url": "/portfolio/static/css/main.02a9a706.chunk.css"
+  },
+  {
+    "revision": "17cf0a2b2a52b621bf0d",
+    "url": "/portfolio/static/js/2.9c605c3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.9c605c3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fb66853ec0fc650242e",
+    "url": "/portfolio/static/js/main.88b28468.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "50a3a36ad80784351342694aded52212",
+    "url": "/portfolio/static/media/profile.50a3a36a.png"
+  },
+  {
+    "revision": "05dbdd06daf2b172f74fe345090736bb",
+    "url": "/portfolio/static/media/restaurant locator.05dbdd06.png"
+  },
+  {
+    "revision": "3552bf5d63dcfcc4d957e594daa43afa",
+    "url": "/portfolio/static/media/robofriends app.3552bf5d.png"
+  },
+  {
+    "revision": "94168837d43ab3a22553effffd8558d8",
+    "url": "/portfolio/static/media/simple restaurant.94168837.png"
+  }
+]);
